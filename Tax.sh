cc29965470e763c7e5ca5019d559c98266ee46b2
Robert Riess <robertwriess@yahoo.com>
2024-04-17 22:43:43 -0400

    commit4

--- Tax.sh
+++ Tax.sh
@@ -5,7 +5,8 @@ Tax () {
 echo "Please enter the total price"
 read item
 
-total = $(item * 8.25)| bc -l
+total=$item
+awk 'BEGIN { y = .08; print "total * y = "(x * y) }'
 
 echo "The items price with tax is $total"
 
